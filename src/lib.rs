2ead0a75d5c5d2e81ce219418f4c13fea8b98d69
joeyame <josephameadows+git@gmail.com>
2022-11-06 18:06:48 -0700

    (U) Continuing to set up the embedded server database

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
-use std::sync::Arc;
-use poem::{EndpointExt, IntoEndpoint};
-use surrealdb::{ Datastore, Error, Session };
-
-// Re-expose so projects don't need imports from surrealdb itself
-// pub use surrealdb::Session;
+use futures_util::{SinkExt, StreamExt};
+use std::{sync::Arc};
+use poem::{EndpointExt, IntoEndpoint, web::{Data, websocket::{WebSocket, Message}}, IntoResponse};
+use surrealdb::{ Datastore, Error, Session, sql::{Part, Value} };
 
 /// Public Types
-pub type SurrealConnection = Arc<Datastore>;
+pub type SurrealDB = Arc<Datastore>;
 pub type SurrealSession = Session;
 
-/// Initiates a SurrealDB database connection from the given URI. URI can
+/// Initiates a SurrealDB database connection from the given URI.
+///  URI can
 /// have the following forms:
 /// 
 /// > `"memory"` initiates the database in RAM
@@ -19,14 +18,123 @@ pub type SurrealSession = Session;
 /// to keep them in their own database directory.
 /// 
 /// > `"tikv://127.0.0.1:2397"` connects to a tikv-backed distributed store.
-pub async fn get_connection( uri: &str ) -> Result<SurrealConnection, Error> {
+pub async fn get_connection( uri: &str, db: &str, ns: &str ) -> Result<SurrealDB, Error> {
     Ok( Arc::new( Datastore::new( &uri ).await? ) )
 }
 
+/// This function initializes an empty database with a ready-to-use user table,
+/// which is compatible with the user authentication functions built into the
+/// javascript client libraries for SurrealDB. If the database is already
+/// configured with a user table, this function won't make any changes.
+pub async fn initiate_auth_db( 
+    db: SurrealDB,
+    db_name: &str, 
+    ns_name: &str,
+) {
+
+    let check_query = "
+        
+    ";
+
+    let ses = Session::for_db( ns_name, db_name );
+    // db.execute(txt, sess, vars, strict)
+}
+
 /// The handler for the SurrealDB connection. Works based on websocket technology
+/// This is handler is designed to work exactly like the /rpc endpoint of the
+/// actual SurrealDB application, but in the Poem web framework, which is a
+/// pleasure to work with.
 #[poem::handler]
-pub fn surreal_socket( ) -> String {
-    "Hello, there!".into()
+fn surreal_socket_handler(
+    ws: WebSocket,
+    db: Data<&SurrealDB>, 
+    ses: Data<&SurrealSession>
+    ) -> impl IntoResponse
+{
+    ws.on_upgrade(move |socket| async move {
+        let ( mut sink, mut stream ) = socket.split();
+        
+        tokio::spawn( async move {
+            while let Some( Ok( msg ) ) = stream.next().await {
+
+                // When a message is received,
+                if let Message::Text( text ) = &msg {
+
+                    // Echo it:
+                    println!( "Websocket client sent: '{}'", text );
+
+                    // Send it right back!
+                    if sink.send( Message::Text( format!("{}", text ) ) ).await.is_err() {
+                        break;
+                    }
+                }
+
+                call( &msg ).await;
+            }
+        })
+    })
+}
+
+async fn call( msg: &Message ) {
+    println!( "Starting message parser!" );
+
+    let msg = msg.clone();
+
+    let req = match msg {
+
+        // Binary message
+        m if m.is_binary() => {
+            todo!();
+        }
+
+        // Text message
+        Message::Text( text ) => {
+            // Guard
+            let Ok( v ) = surrealdb::sql::json( &text ) else {
+                todo!();
+            };
+
+            v
+        }
+
+        // Unsupported message type
+        _ => {
+            todo!();
+        }
+    };
+
+    // Get the 'params' argument
+    let id = match req.pick( &[Part::from( "id" )] ) {
+        v if v.is_none() => None,
+        v if v.is_null() => Some(v),
+        v if v.is_uuid() => Some(v),
+        v if v.is_strand() => Some(v),
+        _ => todo!()
+    };
+
+    // Get the 'method' argument
+    let method = match req.pick( &[Part::from( "method" )]) {
+        Value::Strand( v ) => v.to_raw(),
+        _ => todo!()
+    };
+
+    // Get the 'params' argument
+    let params = match req.pick( &[Part::from( "params" ) ] ) {
+        Value::Array( v ) => v,
+        _ => todo!()
+    };
+
+    println!( "id: {:?}", id );
+    println!( "method: {:?}", method );
+    println!( "params: {:?}", params );
+
+    let res = match &method[..] {
+        "ping" => Value::None,
+        "signin" => {
+            Value::None
+        }
+        _ => todo!()
+    };
 }
 
 /// This function creates the websocket endpoint that SurrealDB's
@@ -36,8 +144,10 @@ pub fn surreal_socket( ) -> String {
 /// #### This should be used as one of Poem's routes:
 /// 
 /// `EXAMPLE HERE`
-pub fn create_surreal_socket_endpoint( db: SurrealConnection, ses: SurrealSession ) -> impl IntoEndpoint {
-    poem::get( surreal_socket ).post( surreal_socket ).data( db ).data( ses )
+pub fn surreal_socket( db: SurrealDB, ses: SurrealSession ) -> impl IntoEndpoint {
+    poem::get( surreal_socket_handler )
+         .post( surreal_socket_handler )
+         .data( db ).data( ses )
 }
 
 pub fn add(left: usize, right: usize) -> usize {
